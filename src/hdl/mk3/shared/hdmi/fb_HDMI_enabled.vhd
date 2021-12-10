e6e06de7eef32af6b051d918673de4a35fc099a0
dominicbeesley <dominic@authorityfile.co.uk>
2021-12-10 01:12:01 +0000

    576i testbed

--- src/hdl/mk3/shared/hdmi/fb_HDMI_enabled.vhd
+++ src/hdl/mk3/shared/hdmi/fb_HDMI_enabled.vhd
@@ -64,55 +64,16 @@ end fb_HDMI;
 
 architecture rtl of fb_hdmi is
 
-	--=========== FISHBONE ============--
-
-	constant PERIPHERAL_COUNT 				: positive := 4;
-	constant PERIPHERAL_N_MEM 				: natural := 0;
-	constant PERIPHERAL_N_VIDPROC 			: natural := 1;
-	constant PERIPHERAL_N_CRTC 				: natural := 2;
-	constant PERIPHERAL_N_I2C					: natural := 3;
+	-- noddy test card gen
 	
-	-- intcon peripheral->controller
-	signal i_per_c2p_intcon				: fb_con_o_per_i_arr(PERIPHERAL_COUNT-1 downto 0);
-	signal i_per_p2c_intcon				: fb_con_i_per_o_arr(PERIPHERAL_COUNT-1 downto 0);
-		-- intcon to peripheral sel
-	signal i_intcon_peripheral_sel_addr	: std_logic_vector(23 downto 0);	
-	signal i_intcon_peripheral_sel			: unsigned(numbits(PERIPHERAL_COUNT)-1 downto 0);  -- address decoded selected peripheral
-	signal i_intcon_peripheral_sel_oh		: std_logic_vector(PERIPHERAL_COUNT-1 downto 0);	-- address decoded selected peripherals as one-hot		
-
-	signal i_ram_fb_m2s					: fb_con_o_per_i_t;
-	signal i_ram_fb_s2m					: fb_con_i_per_o_t;
-	signal i_crtc_fb_m2s					: fb_con_o_per_i_t;
-	signal i_crtc_fb_s2m					: fb_con_i_per_o_t;
-	signal i_vidproc_fb_m2s				: fb_con_o_per_i_t;
-	signal i_vidproc_fb_s2m				: fb_con_i_per_o_t;
-	signal i_i2c_fb_m2s					: fb_con_o_per_i_t;
-	signal i_i2c_fb_s2m					: fb_con_i_per_o_t;
+	signal r_ctr_x							: unsigned(10 downto 0) := (others => '0');
+	signal r_ctr_y							: unsigned(9 downto 0) := (others => '0');
+	signal r_ctr_y_log					: unsigned(9 downto 0) := (others => '0');
 
 
 	-- DVI PLL
 	signal i_clk_hdmi_pixel				: std_logic;
 	signal i_clk_hdmi_tmds				: std_logic;
-
-	--========== LOCAL VIDEO =========--
-	signal i_D_pxbyte 					: std_logic_vector(7 downto 0);
-	signal i_A_pxbyte						: std_logic_vector(16 downto 0);
-
-	signal i_clken_crtc					: std_logic;
-
-	-- RGB signals out of ULA
-	signal i_ULA_R							: std_logic_vector(7 downto 0);
-	signal i_ULA_G							: std_logic_vector(7 downto 0);
-	signal i_ULA_B							: std_logic_vector(7 downto 0);
-
-	-- SYNC signals out of CRTC
-	signal i_vsync_CRTC					: std_logic;
-	signal i_hsync_CRTC					: std_logic;
-	signal i_disen_CRTC					: std_logic;
-	signal i_cursor_CRTC					: std_logic;
-
-	signal i_crtc_MA						: std_logic_vector(13 downto 0);
-	signal i_crtc_RA						: std_logic_vector(4 downto 0);
 
 	signal i_vsync_DVI					: std_logic;
 	signal i_hsync_DVI					: std_logic;
@@ -130,232 +91,6 @@ begin
 	VGA_HS_o <= i_hsync_DVI;
 	VGA_BLANK_o <= i_blank_DVI;
 
-	e_vidproc:entity work.fb_HDMI_vidproc
-	generic map (
-		SIM => SIM
-	)
-	port map(
-		fb_syscon_i		=> fb_syscon_i,
-		fb_c2p_i			=> i_vidproc_fb_m2s,
-		fb_p2c_o			=> i_vidproc_fb_s2m,
-		CLKEN_CRTC_o	=> i_clken_crtc,
-		RAM_D_i			=> i_D_pxbyte,
-		nINVERT_i		=> '1',
-		DISEN_i			=> i_disen_CRTC,
-		CURSOR_i			=> i_cursor_CRTC,
-		R_TTX_i			=> '0',
-		G_TTX_i			=> '0',
-		B_TTX_i			=> '0',
-		R_o				=> i_ULA_R,
-		G_o				=> i_ULA_G,
-		B_o				=> i_ULA_B
-
-	);
-
-	e_crtc:entity work.fb_HDMI_crtc
-	generic map (
-		SIM				=> SIM
-	)
-	port map (
-
-		fb_syscon_i		=> fb_syscon_i,
-		fb_c2p_i			=> i_crtc_fb_m2s,
-		fb_p2c_o			=> i_crtc_fb_s2m,
-		CLKEN_CRTC_i	=> i_clken_crtc,
-		
-		-- Display interface
-		VSYNC_o			=> i_vsync_CRTC,
-		HSYNC_o			=> i_hsync_CRTC,
-		DE_o				=> i_disen_CRTC,
-		CURSOR_o			=> i_cursor_CRTC,
-		LPSTB_i			=> '0',
-		
-		-- Memory interface
-		MA_o				=> i_crtc_MA,
-		RA_o				=> i_crtc_RA
-
-	);
-
-
-
-	e_hdmi_ram:entity work.fb_HDMI_ram
-	generic map (
-		SIM => SIM
-	)
-	port map(
-
-		fb_syscon_i		=> fb_syscon_i,
-		fb_c2p_i			=> i_ram_fb_m2s,
-		fb_p2c_o			=> i_ram_fb_s2m,
-	
-		-- vga signals
-	
-		hdmi_ram_clk_i		=> fb_syscon_i.clk,
-		hdmi_ram_addr_i	=> i_A_pxbyte,
-		hdmi_ram_Q_o		=> i_D_pxbyte
-	
-	);
-
-	e_fb_i2c:entity work.fb_i2c
-	generic map (
-		SIM									=> SIM,
-		CLOCKSPEED							=> CLOCKSPEED
-	)
-	port map (
-
-		-- eeprom signals
-		I2C_SCL_io							=> HDMI_SCL_io,
-		I2C_SDA_io							=> HDMI_SDA_io,
-
-		-- fishbone signals
-
-		fb_syscon_i							=> fb_syscon_i,
-		fb_c2p_i								=> i_i2c_fb_m2s,
-		fb_p2c_o								=> i_i2c_fb_s2m
-	);
-
-
-
---====================================================================
--- DVI 
---====================================================================
-
-	e_synch:entity work.dvi_synchro
-	port map (
-		clk_pixel_dvi => i_clk_hdmi_pixel,
-
-
-		-- input signals in the local clock domain
-		VSYNC_CRTC_i	=> i_vsync_CRTC,
-		HSYNC_CRTC_i	=> i_hsync_CRTC,
-		DISEN_CRTC_i	=> i_disen_CRTC,
-
-		R_ULA_i			=> i_ULA_R,
-		G_ULA_i			=> i_ULA_G,
-		B_ULA_i			=> i_ULA_B,
-
-		-- synchronised / generated / conditioned signals in DVI pixel clock domain
-
-		VSYNC_DVI_o		=> i_vsync_dvi,
-		HSYNC_DVI_o		=> i_hsync_dvi,
-		BLANK_DVI_o		=> i_blank_dvi,
-
-		R_DVI_o			=> i_R_DVI,
-		G_DVI_o			=> i_G_DVI,
-		B_DVI_o			=> i_B_DVI
-
-	);
-
-
-	e_dvid:entity work.dvid
-   port map ( 
-   	clk       => i_clk_hdmi_tmds,
-      clk_pixel => i_clk_hdmi_pixel,
-      red_p     => i_R_dvi,
-      green_p   => i_G_dvi,
-      blue_p    => i_B_dvi,
-      blank     => i_blank_dvi,
-      hsync     => i_hsync_dvi,
-      vsync     => i_vsync_dvi,
-      red_s     => HDMI_R_o,
-      green_s   => HDMI_G_o,
-      blue_s    => HDMI_B_o,
-      clock_s   => HDMI_CK_o
-   );
-
-
---====================================================================
--- Screen address calculations 
---====================================================================
-
--- TODO: improve wrapping (stuck in mode 0..2)
--- TODO: improve teletext detect (out from vidproc?)
-
-
-	-- Address translation logic for calculation of display address
-	process(i_crtc_ma,i_crtc_ra)
-	variable aa : unsigned(3 downto 0);
-	begin
-		if i_crtc_ma(12) = '0' then
-			-- No adjustment
-			aa := unsigned(i_crtc_ma(11 downto 8));
-		else
-				aa := unsigned(i_crtc_ma(11 downto 8)) + 6;
-		end if;
-		
-		if i_crtc_ma(13) = '0' then
-			-- HI RES
-			i_A_pxbyte <= "00" & std_logic_vector(aa(3 downto 0)) & i_crtc_ma(7 downto 0) & i_crtc_ra(2 downto 0);
-		else
-			-- TTX VDU
-			i_A_pxbyte <= "00" & std_logic(aa(3)) & "1111" & i_crtc_ma(9 downto 0);
-		end if;
-	end process;
-
-
-
---====================================================================
--- FISHBONE interconnection
---====================================================================
-
-	i_ram_fb_m2s <= i_per_c2p_intcon(PERIPHERAL_N_MEM);
-	i_vidproc_fb_m2s <= i_per_c2p_intcon(PERIPHERAL_N_VIDPROC);
-	i_crtc_fb_m2s <= i_per_c2p_intcon(PERIPHERAL_N_CRTC);
-	i_i2c_fb_m2s <= i_per_c2p_intcon(PERIPHERAL_N_I2C);
-
-	i_per_p2c_intcon(PERIPHERAL_N_MEM) <= i_ram_fb_s2m;
-	i_per_p2c_intcon(PERIPHERAL_N_VIDPROC) <= i_vidproc_fb_s2m;
-	i_per_p2c_intcon(PERIPHERAL_N_CRTC) <= i_crtc_fb_s2m;
-	i_per_p2c_intcon(PERIPHERAL_N_I2C) <= i_i2c_fb_s2m;
-
-
-	e_fb_intcon: entity work.fb_intcon_one_to_many
-	generic map (
-		SIM 									=> SIM,
-		G_PERIPHERAL_COUNT 						=> PERIPHERAL_COUNT,
-		G_ADDRESS_WIDTH 					=> 24
-		)
-	port map (
-		fb_syscon_i 						=> fb_syscon_i,
-
-		-- peripheral ports connect to controllers
-		fb_con_c2p_i						=> fb_c2p_i,
-		fb_con_p2c_o						=> fb_p2c_o,
-
-		-- controller ports connect to peripherals
-		fb_per_c2p_o						=> i_per_c2p_intcon,
-		fb_per_p2c_i						=> i_per_p2c_intcon,
-
-		peripheral_sel_addr_o					=> i_intcon_peripheral_sel_addr,
-		peripheral_sel_i							=> i_intcon_peripheral_sel,
-		peripheral_sel_oh_i						=> i_intcon_peripheral_sel_oh
-	);
-
-	p_sel:process(i_intcon_peripheral_sel_addr)
-	begin
-		i_intcon_peripheral_sel_oh <= (others => '0');
-
-
-		-- official addresses:
-		-- FB FE00, FE01 - CRTC
-		-- FB FE2x - VIDPROC
-		-- FB FEDx - i2c
-		if i_intcon_peripheral_sel_addr(16 downto 8) = "1" & x"FE" then
-			if i_intcon_peripheral_sel_addr(7) = '1' then
-				i_intcon_peripheral_sel <= to_unsigned(PERIPHERAL_N_I2C, numbits(PERIPHERAL_COUNT));
-				i_intcon_peripheral_sel_oh(PERIPHERAL_N_I2C) <= '1';		
-			elsif i_intcon_peripheral_sel_addr(5) = '1' then
-				i_intcon_peripheral_sel <= to_unsigned(PERIPHERAL_N_VIDPROC, numbits(PERIPHERAL_COUNT));
-				i_intcon_peripheral_sel_oh(PERIPHERAL_N_VIDPROC) <= '1';
-			else
-				i_intcon_peripheral_sel <= to_unsigned(PERIPHERAL_N_CRTC, numbits(PERIPHERAL_COUNT));
-				i_intcon_peripheral_sel_oh(PERIPHERAL_N_CRTC) <= '1';				
-			end if;
-		else
-			i_intcon_peripheral_sel <= to_unsigned(PERIPHERAL_N_MEM, numbits(PERIPHERAL_COUNT));
-			i_intcon_peripheral_sel_oh(PERIPHERAL_N_MEM) <= '1';
-		end if;
-	end process;
 
 
 	g_sim_pll:if SIM generate
@@ -389,6 +124,101 @@ begin
 	end generate;
 
 
+	-- CEA-861-D 1440x576i/50 @27M (21)
+	-- measured from start of blank by line, lines in r_ctr_y are numbered as in CEA-861-D minus 1 i.e. 0 to 624 (instead of 1 to 625)
+
+	p_screengen:process(i_clk_hdmi_pixel)
+	begin
+		if rising_edge(i_clk_hdmi_pixel) then
+			if r_ctr_x = 1727 then
+				r_ctr_x <= (others => '0');
+				if r_ctr_y = 624 then
+					r_ctr_y <= (others => '0');
+				else
+					r_ctr_y <= r_ctr_y + 1;
+				end if;
+			else
+				r_ctr_x <= r_ctr_x + 1;
+			end if;
+		end if;
+	end process;
+
+	i_hsync_DVI <= '0' when r_ctr_x >= 24 and r_ctr_x < 150 else
+						'1';
+
+
+
+	i_vsync_DVI <= '0' when r_ctr_y < 3 or
+								(r_ctr_y = 312 and r_ctr_x >= 863) or
+								r_ctr_y = 313 or
+								r_ctr_y = 314 or
+								(r_ctr_y = 315 and r_ctr_x < 864)
+						else
+						'1';
+
+	i_blank_DVI <= '1' when r_ctr_x < 288 or 
+									r_ctr_y >= 623 or 
+									r_ctr_y < 22 or
+									(r_ctr_y >= 310 and r_ctr_y < 335) else
+						'0';
+
+
+	p_ctr_y_log:process(i_clk_hdmi_pixel)
+	begin
+		if rising_edge(i_clk_hdmi_pixel) then
+			if r_ctr_x = 0 then
+				if r_ctr_y = 22 then
+					r_ctr_y_log <= to_unsigned(0, r_ctr_y_log'length);
+				elsif r_ctr_y = 335 then
+					r_ctr_y_log <= to_unsigned(1, r_ctr_y_log'length);
+				else
+					r_ctr_y_log <= r_ctr_y_log + 2;
+				end if;
+			end if;
+		end if;
+	end process;
+
+	i_R_DVI <= (std_logic_vector(r_ctr_x(5 downto 0)) & "00") when r_ctr_x(4 downto 3) = not r_ctr_y_log(4 downto 3) else
+					(others => '0');
+	i_G_DVI <= std_logic_vector(r_ctr_x(7 downto 0)) when r_ctr_x(6) = '1' else 
+				  	(others => '0');
+	i_B_DVI <= std_logic_vector(r_ctr_y_log(7 downto 0));
+
+
+
+	VGA_R_o <= i_R_DVI(7);
+	VGA_G_o <= i_G_DVI(7);
+	VGA_B_o <= i_B_DVI(7);
+	VGA_VS_o <= i_vsync_DVI;
+	VGA_HS_o <= i_hsync_DVI;
+	VGA_BLANK_o <= i_blank_DVI;
+
+	e_dvid:entity work.dvid
+   port map ( 
+   	clk       => i_clk_hdmi_tmds,
+      clk_pixel => i_clk_hdmi_pixel,
+      red_p     => i_R_dvi,
+      green_p   => i_G_dvi,
+      blue_p    => i_B_dvi,
+      blank     => i_blank_dvi,
+      hsync     => not i_hsync_dvi,
+      vsync     => not i_vsync_dvi,
+      red_s     => HDMI_R_o,
+      green_s   => HDMI_G_o,
+      blue_s    => HDMI_B_o,
+      clock_s   => HDMI_CK_o
+   );
+
+
+
+
+
+--====================================================================
+-- FISHBONE frig
+--====================================================================
+
+	fb_p2c_o.ack <= '1';
+	fb_p2c_o.rdy_ctdn <= RDY_CTDN_MIN;
 
 
 
