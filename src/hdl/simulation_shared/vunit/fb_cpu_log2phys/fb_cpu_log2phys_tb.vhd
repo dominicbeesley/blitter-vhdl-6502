07a90e17b30d25d26eefe41ce652977756a10db3
dominicbeesley <dominic@authorityfile.co.uk>
2022-10-28 15:52:09 +0100

    NEW: multi byte burst read

--- src/hdl/simulation_shared/vunit/fb_cpu_log2phys/fb_cpu_log2phys_tb.vhd
+++ src/hdl/simulation_shared/vunit/fb_cpu_log2phys/fb_cpu_log2phys_tb.vhd
@@ -22,6 +22,8 @@ architecture rtl of test_tb is
 
 	constant CLOCK_PER : time := (1000000/CLOCKSPEED) * 1 ps;
 
+	type t_byte_array is array(natural range <>) of std_logic_vector(7 downto 0);
+
 	signal i_fb_syscon : fb_syscon_t;
 	signal i_fb_con_c2p : fb_con_o_per_i_t;
 	signal i_fb_con_p2c : fb_con_i_per_o_t;
@@ -99,7 +101,7 @@ architecture rtl of test_tb is
 		while i_fb_con_p2c.stall /= '0' loop
 			wait until rising_edge(i_fb_syscon.clk);
 			v_iter := v_iter + 1;
-			if v_iter > 1000 then
+			if v_iter > 100000 then
 				report "Failed waiting for stall" severity error;
 			end if;
 		end loop;
@@ -113,7 +115,7 @@ architecture rtl of test_tb is
 		while i_fb_con_p2c.ack /= '1' loop
 			wait until rising_edge(i_fb_syscon.clk);
 			v_iter := v_iter + 1;
-			if v_iter > 1000 then
+			if v_iter > 100000 then
 				report "Failed waiting for ack" severity error;
 			end if;
 		end loop;
@@ -128,6 +130,65 @@ architecture rtl of test_tb is
 		
 
 	end single_read;
+
+	procedure multi_read(
+			A			: in 	std_logic_vector(23 downto 0);
+			N			: positive;
+		signal c2p	: out	fb_con_o_per_i_t;
+			D    		: out t_byte_array;
+
+			A_stb_dl : natural := 0			-- no of cycles to delay a_stb after cyc and between cycles
+		) is
+	variable v_tx : natural; -- number of a_stb's sent
+	variable v_rx : natural; -- number of acks sent
+	variable v_wt : natural;
+	variable v_tot: natural;
+	variable v_wt_stall:boolean;
+	begin
+
+		c2p <= fb_c2p_unsel;
+
+		wait until rising_edge(i_fb_syscon.clk);
+
+		c2p.cyc <= '1';
+		c2p.rdy_ctdn <= RDY_CTDN_MIN;
+		c2p.we <= '0';
+
+		v_tot := 0;
+		v_wt := A_stb_dl;
+		v_wt_stall := false;
+		while v_rx < N loop
+
+			v_tot := v_tot + 1;
+			assert v_tot < N * 2000 report "multi read " & to_hex_string(A) & "[" & natural'image(N) & "] took too many cyles" severity error;
+
+			if v_tx < N and v_wt = 0 and not v_wt_stall then
+				c2p.A <= std_logic_vector(unsigned(A) + v_tx);
+				c2p.A_stb <= '1';
+				v_tx := v_tx + 1;
+				v_wt_stall := true;
+			end if;
+
+			wait until rising_edge(i_fb_syscon.clk);
+
+			if v_wt_stall and i_fb_con_p2c.stall = '0' then
+				v_wt_stall := false;
+				c2p.A_stb <= '0';
+				v_wt := A_stb_dl;
+			end if;
+
+			if i_fb_con_p2c.ack = '1' then
+				D(v_rx) := i_fb_con_p2c.D_rd;
+				v_rx := v_rx + 1;
+			end if;
+
+		end loop;
+
+		c2p <= fb_c2p_unsel;
+
+
+	end multi_read;
+
 
 	procedure single_write(
 			A			: in 	std_logic_vector(23 downto 0);
@@ -169,7 +230,7 @@ architecture rtl of test_tb is
 				exit;
 			end if;
 			v_iter := v_iter + 1;
-			if v_iter > 1000 then
+			if v_iter > 100000 then
 				report "Failed waiting for stall" severity error;
 			end if;
 		end loop;
@@ -201,7 +262,7 @@ architecture rtl of test_tb is
 		while i_fb_con_p2c.ack /= '1' loop
 			wait until rising_edge(i_fb_syscon.clk);
 			v_iter := v_iter + 1;
-			if v_iter > 1000 then
+			if v_iter > 100000 then
 				report "Failed waiting for ack" severity error;
 			end if;
 		end loop;
@@ -214,6 +275,28 @@ architecture rtl of test_tb is
 	end single_write;
 
 
+	procedure test_multi_mem_read(
+		A				: in std_logic_vector(23 downto 0);
+		N				: positive; -- number of reads in burst
+		signal c2p 	: out	fb_con_o_per_i_t;
+		A_stb_dl		: in natural := 0		-- number of cycles to delay A_stb for reads		
+	)  is
+	variable v_read: t_byte_array(0 to N-1);
+	variable v_exp : t_byte_array(0 to N-1);
+	variable i     : natural;
+	begin
+
+		multi_read(A, N, c2p, v_read, 0);
+
+		for i in 0 to N-1 loop
+			v_exp(I) := std_logic_vector((unsigned(A(7 downto 0)) + i) xor x"FF");
+
+			assert v_read(I) = v_exp(I) report "returned " & to_hex_string(A) & "[" & natural'image(i) & "] " & to_hex_string(v_read(I)) & " expecting " & to_hex_string(v_exp(I)) severity error;
+		end loop;
+
+	end test_multi_mem_read;
+
+
 	procedure test_simple_mem_read(
 		A				: in std_logic_vector(23 downto 0);
 		signal c2p 	: out	fb_con_o_per_i_t;
@@ -222,8 +305,6 @@ architecture rtl of test_tb is
 	variable v_read: std_logic_vector(7 downto 0);
 	variable v_exp : std_logic_vector(7 downto 0);
 	begin
-
-		sim_wait_reset(c2p);
 
 		single_read(A, c2p, v_read, 0);
 
@@ -244,7 +325,6 @@ architecture rtl of test_tb is
 	variable v_read: std_logic_vector(7 downto 0);
 	begin
 
-		sim_wait_reset(c2p);
 
 		single_write(A, c2p, D, A_stb_dl, D_stb_dl);
 
@@ -279,6 +359,7 @@ begin
 
 
 	p_main:process
+	variable v_time:time;
 	begin
 
 		test_runner_setup(runner, runner_cfg);
@@ -289,29 +370,60 @@ begin
 
 				-- simple single read, with no a_stb delay
 
+				sim_wait_reset(i_fb_con_c2p);
 				test_simple_mem_read(x"000000", i_fb_con_c2p, 0);
 
 			elsif run("simple_mem_read2") then
 
 				-- simple single read, with 2 cycle a_stb delay
 
+				sim_wait_reset(i_fb_con_c2p);
 				test_simple_mem_read(x"0001A5", i_fb_con_c2p, 2);
 
 			elsif run("simple_mem_write_then_read") then
 
 				-- simple single write followed by read, with no a_stb/d_stb delay
 
+				sim_wait_reset(i_fb_con_c2p);
 				test_simple_mem_write_then_read(x"000000", x"12", i_fb_con_c2p);
 
 			elsif run("simple_mem_write_then_read2") then
 
 				-- simple single write followed by read, with no a_stb = 2, d_stb = 3 delay
 
+				sim_wait_reset(i_fb_con_c2p);
 				test_simple_mem_write_then_read(x"0001A5", x"BE", i_fb_con_c2p, 2, 3);
+
+			elsif run("ORB") then
+
+				-- check that the VIA delay is working
+
+				sim_wait_reset(i_fb_con_c2p);
+
+				v_time := now;
+
+				test_simple_mem_write_then_read(x"FFFE40", x"BE", i_fb_con_c2p);
+				test_simple_mem_write_then_read(x"FFFE40", x"DD", i_fb_con_c2p);
+
+				v_time := now - v_time;
+
+				report "orb took " & time'image(v_time) severity note;
+
+				assert v_time > 20 us report "ORB too fast" severity error;
+				assert v_time < 25 us report "ORB too slow" severity error;
+
+			elsif run("multi_mem_read") then
+
+				-- simple single read, with no a_stb delay
+
+				sim_wait_reset(i_fb_con_c2p);
+				test_multi_mem_read(x"000000", 4, i_fb_con_c2p, 0);
 
 			end if;
 
 		end loop;
+
+		wait for 3 us;
 
 		test_runner_cleanup(runner); -- Simulation ends here
 	end process;
