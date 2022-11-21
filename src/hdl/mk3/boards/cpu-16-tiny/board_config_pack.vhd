191de0593288b72d2285d6d7d86b69eb28fbb448
dominicbeesley <dominic@authorityfile.co.uk>
2022-11-21 16:51:08 +0000

    tidy

--- src/hdl/mk3/boards/cpu-16-tiny/board_config_pack.vhd
+++ src/hdl/mk3/boards/cpu-16-tiny/board_config_pack.vhd
@@ -61,18 +61,6 @@ package board_config_pack is
 	constant G_INCL_CS_AERIS	: boolean := false;
 	constant G_INCL_CS_EEPROM	: boolean := true;
 
---	constant G_INCL_CPU_T65		: boolean := true;
---	constant G_INCL_CPU_65C02	: boolean := false;
---	constant G_INCL_CPU_6800	: boolean := true;
---	constant G_INCL_CPU_80188	: boolean := true;
---	constant G_INCL_CPU_65816	: boolean := true;
---	constant G_INCL_CPU_6x09	: boolean := true;
---	constant G_INCL_CPU_Z80		: boolean := false;
---	constant G_INCL_CPU_68008	: boolean := false;
---	constant G_INCL_CPU_680x0	: boolean := true;
---	constant G_INCL_CPU_ARM2	: boolean := true;
---
-
 	constant G_INCL_CPU_T65		: boolean := true;
 	constant G_INCL_CPU_65C02	: boolean := true;
 	constant G_INCL_CPU_6800	: boolean := true;
