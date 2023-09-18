25a6930775313e78d1f068334696a18062811a18
dominicbeesley <dominic@authorityfile.co.uk>
2023-09-18 11:57:32 +0100

    rename and setup vunit runner

---
+++ src/hdl/mk3/simulation/vunit/model_bc/general_tb/mk3_model_bc/run.py
@@ -0,0 +1,108 @@
+from vunit import VUnit
+
+# Create VUnit instance by parsing command line arguments
+vu = VUnit.from_argv()
+
+# Create library 'lib'
+lib = vu.add_library("lib")
+
+# Add all files ending in .vhd in current working directory to library
+
+root = "../../../../../../"
+
+# Board specific files
+
+lib.add_source_file(root + "mk3/boards/cpu-16-model-bc/board_config_pack.vhd")
+lib.add_source_file(root + "mk3/boards/cpu-16-model-bc/version.vhd")
+lib.add_source_file(root + "mk3/boards/cpu-16-model-bc/pllmain.vhd")
+
+
+# Base project files
+lib.add_source_file(root + "mk3/shared/hdmi/HDMI_pack.vhd")
+lib.add_source_file(root + "shared/firmware_info_pack.vhd")
+lib.add_source_file(root + "library/fishbone/fishbone_pack.vhd")
+lib.add_source_file(root + "library/fishbone/fb_syscon.vhd")
+lib.add_source_file(root + "library/fishbone/fb_intcon_pack.vhd")
+lib.add_source_file(root + "library/common.vhd")
+lib.add_source_file(root + "library/fishbone/fb_intcon_many_to_one.vhd")
+lib.add_source_file(root + "library/fishbone/fb_intcon_one_to_many.vhd")
+lib.add_source_file(root + "library/fishbone/fb_arbiter_prior.vhd")
+lib.add_source_file(root + "library/fishbone/fb_arbiter_roundrobin.vhd")
+lib.add_source_file(root + "library/fishbone/fb_intcon_shared.vhd")
+lib.add_source_file(root + "library/fishbone/fb_null.vhd")
+lib.add_source_file(root + "library/metadelay.vhd")
+lib.add_source_file(root + "library/clockreg.vhd")
+lib.add_source_file(root + "library/bbc/bbc_slow_cyc.vhd")
+lib.add_source_file(root + "library/3rdparty/T6502/T65_Pack.vhd")
+lib.add_source_file(root + "library/3rdparty/T6502/T65_MCode.vhd")
+lib.add_source_file(root + "library/3rdparty/T6502/T65_ALU.vhd")
+lib.add_source_file(root + "library/3rdparty/T6502/T65.vhd")
+lib.add_source_file(root + "shared/fb_SYS_pack.vhd")
+lib.add_source_file(root + "shared/fb_SYS.vhd")
+lib.add_source_file(root + "shared/fb_SYS_phigen.vhd")
+lib.add_source_file(root + "shared/fb_SYS_VIA_blocker.vhd")
+lib.add_source_file(root + "shared/fb_SYS_clock_dll.vhd")
+lib.add_source_file(root + "shared/fb_VERSION.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_exp_pack.vhd")
+lib.add_source_file(root + "shared/fb_CPU_pack.vhd")
+lib.add_source_file(root + "shared/fb_CPU_t65.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_6x09_exp_pins.vhd")
+lib.add_source_file(root + "shared/fb_CPU_6x09.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_65816_exp_pins.vhd")
+lib.add_source_file(root + "shared/fb_CPU_65816.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_65C02_exp_pins.vhd")
+lib.add_source_file(root + "shared/fb_CPU_65C02.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_680x0_exp_pins.vhd")
+lib.add_source_file(root + "shared/fb_CPU_680x0.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_arm2_exp_pins.vhd")
+lib.add_source_file(root + "shared/fb_CPU_arm2.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_z180_exp_pins.vhd")
+lib.add_source_file(root + "shared/fb_CPU_z180.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_6800_exp_pins.vhd")
+lib.add_source_file(root + "shared/fb_CPU_6800.vhd")
+lib.add_source_file(root + "mk3/shared/fb_CPU_80188_exp_pins.vhd")
+lib.add_source_file(root + "shared/fb_CPU_80188.vhd")
+lib.add_source_file(root + "shared/fb_cpu_log2phys.vhd")
+lib.add_source_file(root + "shared/fb_cpu_con_burst.vhd")
+lib.add_source_file(root + "shared/log2phys.vhd")
+lib.add_source_file(root + "shared/fb_CPU.vhd")
+lib.add_source_file(root + "shared/address_decode.vhd")
+lib.add_source_file(root + "mk3/shared/clocks_pll.vhd")
+lib.add_source_file(root + "shared/fb_i2c.vhd")
+lib.add_source_file(root + "mk3/shared/fb_MEM.vhd")
+lib.add_source_file(root + "shared/fb_memctl.vhd")
+lib.add_source_file(root + "mk3/shared/mk3blit.vhd")
+lib.add_source_file(root + "shared/address_decode_chipset.vhd")
+lib.add_source_file(root + "chipset/fb_chipset_pack.vhd")
+lib.add_source_file(root + "chipset/fb_chipset.vhd")
+lib.add_source_file(root + "chipset/dmac_int_sound.vhd")
+lib.add_source_file(root + "chipset/dmac_int_sound_cha.vhd")
+lib.add_source_file(root + "chipset/dac_1bit.vhd")
+lib.add_source_file(root + "chipset/dmac_int_dma.vhd")
+lib.add_source_file(root + "chipset/dmac_int_dma_cha.vhd")
+
+
+
+# Sim files
+lib.add_source_file(root + "library/common.vhd")
+lib.add_source_file(root + "library/simulation/rom_tb.vhd")
+lib.add_source_file(root + "library/simulation/ram_tb.vhd")
+lib.add_source_file(root + "library/simulation/ls02.vhd")
+lib.add_source_file(root + "library/simulation/ls04.vhd")
+lib.add_source_file(root + "library/simulation/ls32.vhd")
+lib.add_source_file(root + "library/simulation/ls51.vhd")
+lib.add_source_file(root + "library/simulation/ls74.vhd")
+lib.add_source_file(root + "library/simulation/bbc/bbc_clock_gen.vhd")
+lib.add_source_file(root + "library/bbc/bbc_slow_cyc.vhd")
+lib.add_source_file(root + "library/simulation/bbc/elk_clock_gen.vhd")
+lib.add_source_file(root + "library/bbc/elk_slow_cyc.vhd")
+lib.add_source_file(root + "simulation_shared/sim_SYS_pack.vhd")
+lib.add_source_file(root + "simulation_shared/sim_SYS_tb.vhd")
+
+lib.add_source_file(root + "mk3/simulation/sim_tb/sim_t65_model_bc_tb.vhd")
+
+tb = lib.test_bench("sim_t65_model_bc_tb")
+tb.set_generic("G_MOSROMFILE","../../../../../../../sim_asm/test_asm_model_BC/build/model_bc.rom")
+
+# Run vunit function
+vu.main()
