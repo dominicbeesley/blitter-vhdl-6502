b89fcecc5155822b8687c922cc5aa5128a205133
dominicbeesley <dominicbeesley@gmail.com>
2024-11-06 16:44:10 +0000

    update screenshots for new BLTUTIL rom

--- release/release-script.sh
+++ release/release-script.sh
@@ -15,23 +15,23 @@ CODE65=/mnt/c/Users/Dominic/Documents/GitHub/blitter-65xx-code/build/ssds
 SSDS_65="roms65 tools65 demo65 adventure bigfonts examblit paula"
 SSDS="z80 bas816 roms69"
 
-ROMS65_SRC=${HOSTFS}/roms65
-ROMS65_ITEMS="ADFSH30.inf MOS120.M.inf BLMMFS.rom.inf BLTUTIL.inf OSTEST.M.inf basic2.rom.inf BASIC432.rom.inf SWMMFS.rom.inf"
+##ROMS65_SRC=${HOSTFS}/roms65
+##ROMS65_ITEMS="ADFSH30.inf MOS120.M.inf BLMMFS.rom.inf BLTUTIL.inf OSTEST.M.inf basic2.rom.inf BASIC432.rom.inf SWMMFS.rom.inf"
 
-TOOLS65_SRC=${HOSTFS}/tools65
-TOOLS65_ITEMS="CLOCKDP.inf CLOCKSP.inf FLSHTST.inf I2CDUMP.inf JIMTEST.inf MEMSZ.inf"
+##TOOLS65_SRC=${HOSTFS}/tools65
+##TOOLS65_ITEMS="CLOCKDP.inf CLOCKSP.inf FLSHTST.inf I2CDUMP.inf JIMTEST.inf MEMSZ.inf"
 
-DEMO65_SRC=${HOSTFS}/demo65
-DEMO65_ITEMS="BACK.S.inf FONT.D.inf FONT.M.inf FONT.S.inf HDPAL.inf MENU.inf MODPLAY.inf TEST.inf TEST0.inf TEST1.inf TEST2.inf TEST3.inf TEST4.inf TEST5.inf _21BOOT.inf"
-DEMO65_OPT4=3
+##DEMO65_SRC=${HOSTFS}/demo65
+##DEMO65_ITEMS="BACK.S.inf FONT.D.inf FONT.M.inf FONT.S.inf HDPAL.inf MENU.inf MODPLAY.inf TEST.inf TEST0.inf TEST1.inf TEST2.inf TEST3.inf TEST4.inf TEST5.inf _21BOOT.inf"
+##DEMO65_OPT4=3
 
-ADVENT65_SRC=${HOSTFS}/advent65
-ADVENT65_ITEMS="CHARAC.T.inf CLIB.R.inf GAME.inf HOME.M.inf LOADER.inf MAIN.P.inf OBACK.T.inf OCOLL.T.inf OFRONT.T.inf OVER.M.inf WANDER.inf _21BOOT.inf"
-ADVENT65_OPT4=3
+##ADVENT65_SRC=${HOSTFS}/advent65
+##ADVENT65_ITEMS="CHARAC.T.inf CLIB.R.inf GAME.inf HOME.M.inf LOADER.inf MAIN.P.inf OBACK.T.inf OCOLL.T.inf OFRONT.T.inf OVER.M.inf WANDER.inf _21BOOT.inf"
+##ADVENT65_OPT4=3
 
-BIGFONTS_SRC=${HOSTFS}/bigfonts
-BIGFONTS_ITEMS="CLIB.R.inf DEMO.inf FONT.T.inf LOADER.inf MAIN.P.inf OWL.T.inf _21BOOT.inf"
-BIGFONTS_OPT4=3
+##BIGFONTS_SRC=${HOSTFS}/bigfonts
+##BIGFONTS_ITEMS="CLIB.R.inf DEMO.inf FONT.T.inf LOADER.inf MAIN.P.inf OWL.T.inf _21BOOT.inf"
+##BIGFONTS_OPT4=3
 
 ROMS69_SRC=${HOSTFS}/roms69
 ROMS69_ITEMS="6309BAS.bin.inf 6368BAS.bin.inf 6809BAS.bin.inf HOSTFS-myelin.bin.inf HOSTFS.bin.inf mosrom-6809.bin.inf mosrom-nat.bin.inf mosrom.bin.inf UTILS.bin.inf"
@@ -71,6 +71,9 @@ for ssd in ${SSDS}; do
 	fi
 	for r in ${_ITEMS}; do
 		echo " - ${r}"
+		if [[ ! -e "${_SRC}/${r}" ]]; then
+			echo "Missing file ${_SRC}/${r}" 1>&2;
+		fi
 		dfs add ${_SSD} ${_SRC}/${r}
 	done;
 done
