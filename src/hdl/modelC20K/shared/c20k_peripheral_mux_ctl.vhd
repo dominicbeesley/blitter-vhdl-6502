aaa7e6aaab0552911de5f57a31c5212b686d2f8f
dominicbeesley <dominicbeesley@gmail.com>
2025-06-10 16:59:52 +0100

    full test - pass through to board and get back out, correct MIO clashes at 96M

--- src/hdl/modelC20K/shared/c20k_peripheral_mux_ctl.vhd
+++ src/hdl/modelC20K/shared/c20k_peripheral_mux_ctl.vhd
@@ -70,27 +70,26 @@ port (
 
    -- address and cycle selection from core
    -- this needs to be ready before the ALE phase
-   sys_A_en                : in     std_logic;                       -- ignore cycle when '0'
+   sys_cyc_en_i            : in     std_logic;                       -- ignore SYS bus cycle when '0'
    sys_A_i                 : in     std_logic_vector(15 downto 0);   -- this will be decoded to a peripheral
-   sys_RnW_i               : in     std_logic;
+   sys_RnW_i               : in     std_logic;                       -- bus/peripherals RnW
+   sys_nRST_i              : in     std_logic;                       -- bus/peripherals reset
 
    -- address and cycle selection back to core
    addr_ack_clken_o        : out    std_logic;                       -- clken for an address being accepted 
                                                                      -- from the core into this module
+   -- sys write 
+   sys_D_wr_i              : in     std_logic_vector(7 downto 0);    -- needs to be ready before write part of cycle
 
    -- data and inputs back from bus at end of cycle
-   sys_D_rd                : out    std_logic_vector(7 downto 0);    -- data back from peripherals
-   sys_D_rd_clken          : out    std_logic;                       -- read data valid
-
-   
+   sys_D_rd_o              : out    std_logic_vector(7 downto 0);    -- data back from peripherals
+   sys_D_rd_clken_o        : out    std_logic;                       -- read data valid
 
    -- mux clock outputs
-   mux_mhz1E_clk           : out    std_logic;                        -- 1MHzE clock for main board
-   mux_mhz2E_clk           : out    std_logic;                        -- 2MHzE clock for main board - cycle stretched
+   mux_mhz1E_clk_o         : out    std_logic;                        -- 1MHzE clock for main board
+   mux_mhz2E_clk_o         : out    std_logic;                        -- 2MHzE clock for main board - cycle stretched
 
    -- mux control outputs
-
-
    mux_nALE_o              : out    std_logic;
    mux_D_nOE_o             : out    std_logic;
    mux_I0_nOE_o            : out    std_logic;
@@ -99,8 +98,41 @@ port (
    mux_O1_nOE_o            : out    std_logic;
 
    -- mux multiplexed signals bus
-   
-   mux_bus_io              : inout  std_logic_vector(7 downto 0)
+   mux_bus_io              : inout  std_logic_vector(7 downto 0);
+
+   -- random other multiplexed pins out to FPGA (I0 phase)
+   p_ser_cts_o             : out    std_logic;
+   p_ser_rx_o              : out    std_logic;
+   p_d_cas_o               : out    std_logic;
+   p_kb_nRST_o             : out    std_logic;
+   p_kb_CA2_o              : out    std_logic;
+   p_netint_o              : out    std_logic;
+   p_irq_o                 : out    std_logic;
+   p_nmi_o                 : out    std_logic;
+
+   -- random other multiplexed pins out to FPGA (I1 phase)
+   p_j_i0_o                : out    std_logic;
+   p_j_i1_o                : out    std_logic;
+   p_j_spi_miso_o          : out    std_logic;
+   p_btn0_o                : out    std_logic;
+   p_btn1_o                : out    std_logic;
+   p_btn2_o                : out    std_logic;
+   p_btn3_o                : out    std_logic;
+   p_kb_pa7_o              : out    std_logic;
+
+   -- random other multiplexed pins in from FPGA (O0 phase)
+   p_SER_TX_i              : in     std_logic;
+   p_SER_RTS_i             : in     std_logic;
+
+   -- random other multiplexed pins in from FPGA (O1 phase)
+   p_j_ds_nCS2_i           : in     std_logic;
+   p_j_ds_nCS1_i           : in     std_logic;
+   p_j_spi_clk_i           : in     std_logic;
+   p_VID_HS_i              : in     std_logic;
+   p_VID_VS_i              : in     std_logic;
+   p_VID_CS_i              : in     std_logic;
+   p_j_spi_mosi_i          : in     std_logic;
+   p_j_adc_nCS_i           : in     std_logic
 
 
 );
@@ -108,13 +140,13 @@ end c20k_peripherals_mux_ctl;
 
 architecture rtl of c20k_peripherals_mux_ctl is
    
-   constant C_CLKS_MHZ2          : natural := G_FAST_CLOCKSPEED / 4000000;
+   constant C_CLKS_MHZ2          : natural := G_FAST_CLOCKSPEED / 2000000;
    constant C_CLKS_MHZ2_HALF     : natural := C_CLKS_MHZ2 / 2;
 
    constant C_CTR2_LENGTH        : natural := numbits(C_CLKS_MHZ2 - 1);
 
    --imaginary 32MHz slices within a 2MHz cycle
-   constant C_32_D_hold          : natural := 1;
+   constant C_32_D_hold          : natural := 0;
    constant C_32_ALE             : natural := 2;
    constant C_32_O0              : natural := 3;
    constant C_32_I0              : natural := 4;
@@ -129,6 +161,15 @@ architecture rtl of c20k_peripherals_mux_ctl is
          return 3;
       else
          return 4;
+      end if;
+   end function;
+
+   function C_F_SOFF return natural is
+   begin
+      if G_BEEBFPGA then
+         return 1;
+      else
+         return 2;
       end if;
    end function;
 
@@ -156,6 +197,34 @@ architecture rtl of c20k_peripherals_mux_ctl is
    type slow_cyc_t is (fast, slow_short, slow_short_2, slow_long);
    signal r_slow                 : slow_cyc_t   := fast;
 
+   -- convenience and grouping of mux bus signals
+   signal i_MIO_O0               : std_logic_vector(7 downto 0);
+   signal i_MIO_O1               : std_logic_vector(7 downto 0);
+   signal i_MIO_nCS              : std_logic_vector(3 downto 0);
+
+   -- registered signals in from multiplexed input phases
+   signal r_p_ser_cts            : std_logic;
+   signal r_p_ser_rx             : std_logic;
+   signal r_p_d_cas              : std_logic;
+   signal r_p_kb_nRST            : std_logic;
+   signal r_p_kb_CA2             : std_logic;
+   signal r_p_netint             : std_logic;
+   signal r_p_irq                : std_logic;
+   signal r_p_nmi                : std_logic;
+   signal r_p_j_i0               : std_logic;
+   signal r_p_j_i1               : std_logic;
+   signal r_p_j_spi_miso         : std_logic;
+   signal r_p_btn0               : std_logic;
+   signal r_p_btn1               : std_logic;
+   signal r_p_btn2               : std_logic;
+   signal r_p_btn3               : std_logic;
+   signal r_p_kb_pa7             : std_logic;
+
+
+   -- bus registered signals
+   signal r_SYS_A                : std_logic_vector(15 downto 0)  := (others => '0');
+   signal r_SYS_RnW              : std_logic                      := '1';
+   signal r_SYS_D_wr             : std_logic_vector(7 downto 0)   := (others => '0');
 
 
 begin
@@ -212,7 +281,7 @@ begin
          else
             if to_integer(r_mhz2_ctdn) = C_F_ALE and r_slow = fast then
                -- register whether a slow or fast cycle and the type
-               if i_bbc_slow_cyc then
+               if i_bbc_slow_cyc = '1' and sys_cyc_en_i = '1' then
                   if r_mhz1E_clk = '1' then
                      r_slow <= slow_long;
                   else
@@ -247,21 +316,155 @@ begin
       end if;
    end process;
 
+   mhz1E_clken_o <= r_mhz1E_clken;
+   mhz2E_clken_o <=  r_mhz2int_clken when r_slow = fast else 
+                     '0';
+   mux_mhz1E_clk_o <= r_mhz1E_clk;
+   mux_mhz2E_clk_o <= r_mhz2E_clk;
+
+
    p_cyc:process(clk_fast_i)
    begin
       if rising_edge(clk_fast_i) then
          addr_ack_clken_o <= '0';
-         if to_integer(r_mhz2_ctdn) = C_F_ALE and r_slow = fast then
-            addr_ack_clken_o <= '1';
+         if reset_i = '1' then
+            r_SYS_A <= (others => '0');
+            r_SYS_RnW <= '1';
+         else
+            if to_integer(r_mhz2_ctdn) = C_F_ALE-1 and r_slow = fast then
+               if sys_cyc_en_i = '1' then
+                  addr_ack_clken_o <= '1';
+                  r_SYS_A <= sys_A_i;
+                  r_SYS_RnW <= sys_RnW_i;
+               else
+                  r_SYS_A <= x"FFEA";
+                  r_SYS_RnW <= '1';
+               end if;
+            end if;
          end if;
       end if;
    end process;
 
-   mhz1E_clken_o <= r_mhz1E_clken;
-   mhz2E_clken_o <=  r_mhz2int_clken when r_slow = fast else 
-                     '0';
-   mux_mhz1E_clk <= r_mhz1E_clk;
-   mux_mhz2E_clk <= r_mhz2E_clk;
+   p_wr:process(clk_fast_i)
+   begin
+      if rising_edge(clk_fast_i) then
+         if reset_i = '1' then
+            r_SYS_D_wr <= (others => '0');
+         else
+            if to_integer(r_mhz2_ctdn) = C_F_D_write - 1 then
+               r_SYS_D_wr <= sys_D_wr_i;
+            end if;
+         end if;
+      end if;      
+   end process;
+
+   p_reg_i0:process(clk_fast_i)
+   begin
+      if rising_edge(clk_fast_i) then
+         if to_integer(r_mhz2_ctdn) = C_F_I0 + 2 then
+            r_p_ser_cts <= mux_bus_io(0);
+            r_p_ser_rx <= mux_bus_io(1);
+            r_p_d_cas <= mux_bus_io(2);
+            r_p_kb_nRST <= mux_bus_io(3);
+            r_p_kb_CA2 <= mux_bus_io(4);
+            r_p_netint <= mux_bus_io(5);
+            r_p_irq <= mux_bus_io(6);
+            r_p_nmi <= mux_bus_io(7);
+         end if;
+      end if;
+   end process;
+
+   p_reg_i1:process(clk_fast_i)
+   begin
+      if rising_edge(clk_fast_i) then
+         if to_integer(r_mhz2_ctdn) = C_F_I1 + 2 then
+            r_p_j_i0 <= mux_bus_io(0);
+            r_p_j_i1 <= mux_bus_io(1);
+            r_p_j_spi_miso <= mux_bus_io(2);
+            r_p_btn0 <= mux_bus_io(3);
+            r_p_btn1 <= mux_bus_io(4);
+            r_p_btn2 <= mux_bus_io(5);
+            r_p_btn3 <= mux_bus_io(6);
+            r_p_kb_pa7  <= mux_bus_io(7);
+         end if;
+      end if;
+   end process;
+
+   p_ser_cts_o <= r_p_ser_cts;
+   p_ser_rx_o <= r_p_ser_rx;
+   p_d_cas_o <= r_p_d_cas;
+   p_kb_nRST_o <= r_p_kb_nRST;
+   p_kb_CA2_o <= r_p_kb_CA2;
+   p_netint_o <= r_p_netint;
+   p_irq_o <= r_p_irq;
+   p_nmi_o <= r_p_nmi;
+   p_j_i0_o <= r_p_j_i0;
+   p_j_i1_o <= r_p_j_i1;
+   p_j_spi_miso_o <= r_p_j_spi_miso;
+   p_btn0_o <= r_p_btn0;
+   p_btn1_o <= r_p_btn1;
+   p_btn2_o <= r_p_btn2;
+   p_btn3_o <= r_p_btn3;
+   p_kb_pa7_o <= r_p_kb_pa7;
+
+
+   mux_nALE_o  <=    '0'   when to_integer(r_mhz2_ctdn) = C_F_ALE else
+                     '1';
+   mux_O0_nOE_o <=   '0'   when to_integer(r_mhz2_ctdn) = C_F_O0 else
+                     '1';
+   mux_O1_nOE_o <=   '0'   when to_integer(r_mhz2_ctdn) = C_F_O1 else
+                     '1';
+
+   mux_I0_nOE_o <=   '0'   when to_integer(r_mhz2_ctdn) >= C_F_I0 and to_integer(r_mhz2_ctdn) < C_F_I0 + C_F_MUL-1 else
+                     '1';
+   mux_I1_nOE_o <=   '0'   when to_integer(r_mhz2_ctdn) >= C_F_I1 and to_integer(r_mhz2_ctdn) < C_F_I1 + C_F_MUL-1 else
+                     '1';
+
+   i_MIO_nCS <= "1010"  when r_SYS_A(15 downto 8) = x"FC" else        -- PGFC -- TODO: local holes
+                "1011"  when r_SYS_A(15 downto 8) = x"FD" else        -- PGFD -- TODO: local holes/jim paging reg
+                "1100"  when r_SYS_A(15 downto 5) & "0" = x"FEE" else -- TUBE
+                "1101"  when r_SYS_A(15 downto 5) & "0" = x"FEA" else -- ADLC
+                "0110"  when r_SYS_A(15 downto 5) & "0" = x"FE8" and r_SYS_A(2) = '1' else -- FDC
+                "0111"  when r_SYS_A(15 downto 5) & "0" = x"FE8" and r_SYS_A(2) = '0' and r_SYS_RnW = '0' else -- FDCON
+                "1001"  when r_SYS_A(15 downto 5) & "0" = x"FE6" else -- VIAB
+                "0100"  when r_SYS_A(15 downto 0)       = x"FE41" and r_SYS_RnW = '0' else -- KBPAWR
+                "0100"  when r_SYS_A(15 downto 0)       = x"FE4F" and r_SYS_RnW = '0' else -- KBPAWR
+                "0101"  when r_SYS_A(15 downto 0)       = x"FE40" and r_SYS_RnW = '0' else -- IC32WR
+                "0000";
+
+   i_MIO_O0 <= (
+         3 downto 0 => i_MIO_nCS,
+         4 => r_SYS_RnW,
+         5 => SYS_nRST_i,
+         6 => p_SER_TX_i,
+         7 => p_SER_RTS_i
+      );
+
+   i_MIO_O1 <= (
+      0 => p_J_DS_nCS2_i,
+      1 => p_J_DS_nCS1_i,
+      2 => p_VID_CS_i,
+      3 => p_VID_VS_i,
+      4 => p_VID_HS_i,
+      5 => p_J_ADC_nCS_i,
+      6 => p_J_SPI_MOSI_i,
+      7 => p_J_SPI_CLK_i
+      );
+
+
+   mux_bus_io <=  r_SYS_D_wr  when to_integer(r_mhz2_ctdn) <= C_F_D_hold + C_F_MUL - 1 and r_SYS_RnW = '0' else
+                  r_SYS_A(7 downto 0)     
+                              when to_integer(r_mhz2_ctdn) >= C_F_ALE and to_integer(r_mhz2_ctdn) < C_F_ALE + C_F_MUL-1 else
+                  i_MIO_O0    when to_integer(r_mhz2_ctdn) >= C_F_O0 and to_integer(r_mhz2_ctdn) < C_F_O0 + C_F_MUL-1 else
+                  i_MIO_O1    when to_integer(r_mhz2_ctdn) >= C_F_O1 and to_integer(r_mhz2_ctdn) < C_F_O1 + C_F_MUL-1 else
+                  r_SYS_D_wr  when to_integer(r_mhz2_ctdn) >= C_F_D_write and r_SYS_RnW = '0' else
+                  (others => 'Z');
+
+   mux_D_nOE_o  <= '0'  when to_integer(r_mhz2_ctdn) <= C_F_D_hold + C_F_MUL - 1 else
+                   '0'  when to_integer(r_mhz2_ctdn) >= C_F_D_write and r_SYS_RnW = '0' else
+                   '0'  when to_integer(r_mhz2_ctdn) >= C_F_D_read  and r_SYS_RnW = '1' else
+                   '1';
+
 
 
    e_slow_cyc:entity work.bbc_slow_cyc
@@ -269,9 +472,5 @@ begin
       sys_A_i        => sys_A_i,
       slow_o         => i_bbc_slow_cyc
    );
-
-
-
-   mux_bus_io <= (others => 'Z');
 
 end rtl;
--- src/hdl/modelC20K/vunit/tests/sim_beebfpga_peripherals_obs/test_tb.vhd
+++ src/hdl/modelC20K/vunit/tests/sim_beebfpga_peripherals_obs/test_tb.vhd
@@ -30,6 +30,7 @@ architecture rtl of test_tb is
    signal i_SYS_RnW        : std_logic;
    signal i_SYS_A          : std_logic_vector(15 downto 0);
    signal i_sys_D_wr       : std_logic_vector(7 downto 0);
+   signal i_sys_nRST       : std_logic;
 
    signal ic_addr_ack_clken: std_logic;
 
@@ -48,14 +49,87 @@ architecture rtl of test_tb is
 
    signal im_mux_bus_io    : std_logic_vector(7 downto 0);
 
+   -- peripheral signals to / from FPGA to controller
+   signal ipi_ser_cts      : std_logic;
+   signal ipi_ser_rx       : std_logic;
+   signal ipi_d_cas        : std_logic;
+   signal ipi_kb_nRST      : std_logic;
+   signal ipi_kb_CA2       : std_logic;
+   signal ipi_netint       : std_logic;
+   signal ipi_irq          : std_logic;
+   signal ipi_nmi          : std_logic;
+   signal ipi_j_i0         : std_logic;
+   signal ipi_j_i1         : std_logic;
+   signal ipi_j_spi_miso   : std_logic;
+   signal ipi_btn0         : std_logic;
+   signal ipi_btn1         : std_logic;
+   signal ipi_btn2         : std_logic;
+   signal ipi_btn3         : std_logic;
+   signal ipi_kb_pa7       : std_logic;
+
+   signal ipo_SER_TX       : std_logic;
+   signal ipo_SER_RTS      : std_logic;
+   signal ipo_j_ds_nCS2    : std_logic;
+   signal ipo_j_ds_nCS1    : std_logic;
+   signal ipo_j_spi_clk    : std_logic;
+   signal ipo_VID_HS       : std_logic;
+   signal ipo_VID_VS       : std_logic;
+   signal ipo_VID_CS       : std_logic;
+   signal ipo_j_spi_mosi   : std_logic;
+   signal ipo_j_adc_nCS    : std_logic;
+
+   -- peripheral signals on the simulated motherboard in/out of the multiplexer
+   signal ibpo_RnW         : std_logic;
+   signal ibpo_nRST        : std_logic;
+   signal ibpo_SER_TX      : std_logic;
+   signal ibpo_SER_RTS     : std_logic;
+   signal ibpo_nADLC       : std_logic;
+   signal ibpo_nKBPAWR     : std_logic;
+   signal ibpo_nIC32WR     : std_logic;
+   signal ibpo_nPGFC       : std_logic;
+   signal ibpo_nPGFD       : std_logic;
+   signal ibpo_nFDC        : std_logic;
+   signal ibpo_nTUBE       : std_logic;
+   signal ibpo_nFDCONWR    : std_logic;
+   signal ibpo_nVIAB       : std_logic;
+   signal ibpi_ser_cts     : std_logic;
+   signal ibpi_ser_rx      : std_logic;
+   signal ibpi_d_cas       : std_logic;
+   signal ibpi_kb_nRST     : std_logic;
+   signal ibpi_kb_CA2      : std_logic;
+   signal ibpi_netint      : std_logic;
+   signal ibpi_irq         : std_logic;
+   signal ibpi_nmi         : std_logic;
+   signal ibpo_j_ds_nCS2   : std_logic;
+   signal ibpo_j_ds_nCS1   : std_logic;
+   signal ibpo_j_spi_clk   : std_logic;
+   signal ibpo_VID_HS      : std_logic;
+   signal ibpo_VID_VS      : std_logic;
+   signal ibpo_VID_CS      : std_logic;
+   signal ibpo_j_spi_mosi  : std_logic;
+   signal ibpo_j_adc_nCS   : std_logic;
+   signal ibpi_j_i0        : std_logic;
+   signal ibpi_j_i1        : std_logic;
+   signal ibpi_j_spi_miso  : std_logic;
+   signal ibpi_btn0        : std_logic;
+   signal ibpi_btn1        : std_logic;
+   signal ibpi_btn2        : std_logic;
+   signal ibpi_btn3        : std_logic;
+   signal ibpi_kb_pa7      : std_logic;
+   signal ibpio_P_D        : std_logic_vector(7 downto 0);
+   signal ibpo_A           : std_logic_vector(7 downto 0);
+
+
+
 begin
+
 
    p_clk_48:process
    begin
       i_clk_48 <= '0';
-      wait for 0.5 us / 48;
+      wait for 0.5 us / 96;
       i_clk_48 <= '1';
-      wait for 0.5 us / 48;
+      wait for 0.5 us / 96;
    end process;
 
 
@@ -94,12 +168,46 @@ begin
 
          if run("test") then
 
-            i_SYS_A <= (others => '0');
+            -- reset inputs to mux
+            ibpi_ser_cts <= '0';
+            ibpi_ser_rx <= '0';
+            ibpi_d_cas <= '0';
+            ibpi_kb_nRST <= '0';
+            ibpi_kb_CA2 <= '0';
+            ibpi_netint <= '0';
+            ibpi_irq <= '0';
+            ibpi_nmi <= '0';
+            ibpi_j_i0 <= '0';
+            ibpi_j_i1 <= '0';
+            ibpi_j_spi_miso <= '0';
+            ibpi_btn0 <= '0';
+            ibpi_btn1 <= '0';
+            ibpi_btn2 <= '0';
+            ibpi_btn3 <= '0';
+            ibpi_kb_pa7 <= '0';
 
+            -- reset outputs from fpga
+            ipo_SER_TX <= '0';
+            ipo_SER_RTS <= '0';
+            ipo_j_ds_nCS2 <= '0';
+            ipo_j_ds_nCS1 <= '0';
+            ipo_j_spi_clk <= '0';
+            ipo_VID_HS <= '0';
+            ipo_VID_VS <= '0';
+            ipo_VID_CS <= '0';
+            ipo_j_spi_mosi <= '0';
+            ipo_j_adc_nCS <= '0';
+
+            i_SYS_A <= (others => '0');
+            i_SYS_RnW <= '1';
+            i_SYS_D_wr <= (others => '0');
+            
+            i_sys_nRST <= '0';
             i_reset <= '1';
             wait for 1 us;
             wait until rising_edge(i_clk_48);
             i_reset <= '0';
+            i_sys_nRST <= '1';
 
             wait until ic_mhz2E_clken = '1' and rising_edge(i_clk_48);
             wait until ic_mhz2E_clken = '1' and rising_edge(i_clk_48);
@@ -117,7 +225,11 @@ begin
             CYC_R(x"FFEA");
             CYC_R(x"FFEA");
             CYC_W(x"FE80", x"A5");      -- FDCON
+            ibpi_btn0 <= '1';
+            ibpi_btn2 <= '1';
             CYC_W(x"FE80", x"5A");
+            ibpi_btn0 <= '0';
+            ibpi_btn2 <= '0';
             CYC_W(x"FE84", x"A5");      -- FDC
             CYC_W(x"FE84", x"5A");
 
@@ -154,20 +266,23 @@ begin
       reset_i                 => i_reset,
 
       -- address and cycle selection from core
-      sys_A_en                => '1',
+      sys_cyc_en_i            => '1',
       sys_A_i                 => i_SYS_A,
       sys_RnW_i               => i_SYS_RnW,
+      sys_nRST_i              => i_sys_nRST,
 
       -- address and cycle selection back to core
       addr_ack_clken_o        => ic_addr_ack_clken,
 
+      sys_D_wr_i              => i_sys_D_wr,
+
       -- data and inputs back from bus at end of cycle
-      sys_D_rd                => i_sys_D_rd,
-      sys_D_rd_clken          => i_sys_D_rd_clken,
+      sys_D_rd_o              => i_sys_D_rd,
+      sys_D_rd_clken_o        => i_sys_D_rd_clken,
 
       -- mux clock outputs
-      mux_mhz1E_clk            => im_mux_mhz1E_clk,
-      mux_mhz2E_clk            => im_mux_mhz2E_clk,
+      mux_mhz1E_clk_o         => im_mux_mhz1E_clk,
+      mux_mhz2E_clk_o         => im_mux_mhz2E_clk,
 
       -- mux control outputs
       mux_nALE_o              => im_mux_nALE_o,
@@ -178,79 +293,119 @@ begin
       mux_O1_nOE_o            => im_mux_O1_nOE_o,
 
       -- mux multiplexed signals bus   
-      mux_bus_io              => im_mux_bus_io
+      mux_bus_io              => im_mux_bus_io,
+
+      -- random other multiplexed pins out to FPGA (I0 phase)
+      p_ser_cts_o             => ipi_ser_cts,
+      p_ser_rx_o              => ipi_ser_rx,
+      p_d_cas_o               => ipi_d_cas,
+      p_kb_nRST_o             => ipi_kb_nRST,
+      p_kb_CA2_o              => ipi_kb_CA2,
+      p_netint_o              => ipi_netint,
+      p_irq_o                 => ipi_irq,
+      p_nmi_o                 => ipi_nmi,
+
+      -- random other multiplexed pins out to FPGA (I1 phase)
+      p_j_i0_o                => ipi_j_i0,
+      p_j_i1_o                => ipi_j_i1,
+      p_j_spi_miso_o          => ipi_j_spi_miso,
+      p_btn0_o                => ipi_btn0,
+      p_btn1_o                => ipi_btn1,
+      p_btn2_o                => ipi_btn2,
+      p_btn3_o                => ipi_btn3,
+      p_kb_pa7_o              => ipi_kb_pa7,
+
+      -- random other multiplexed pins in from FPGA (O0 phase)
+      p_SER_TX_i              => ipo_SER_TX,
+      p_SER_RTS_i             => ipo_SER_RTS,
+
+      -- random other multiplexed pins in from FPGA (O1 phase)
+      p_j_ds_nCS2_i           => ipo_j_ds_nCS2,
+      p_j_ds_nCS1_i           => ipo_j_ds_nCS1,
+      p_j_spi_clk_i           => ipo_j_spi_clk,
+      p_VID_HS_i              => ipo_VID_HS,
+      p_VID_VS_i              => ipo_VID_VS,
+      p_VID_CS_i              => ipo_VID_CS,
+      p_j_spi_mosi_i          => ipo_j_spi_mosi,
+      p_j_adc_nCS_i           => ipo_j_adc_nCS
+
+
    );
 
 
+--===========================================================
+-- board sim
+--===========================================================
 
---   e_brd_per:entity work.sim_peripherals_mux
---   port map (
---
---      clk_2MHz_E_i   => i_clk_2MHzE,
---      clk_1MHz_E_i   => i_clk_1MHzE,
---      
---      MIO_io         => i_MIO,
---      MIO_nALE_i     => i_MIO_nALE,
---      MIO_D_nOE_i    => i_MIO_D_nOE,
---      MIO_I0_nOE_i   => i_MIO_I0_nOE,
---      MIO_I1_nOE_i   => i_MIO_I1_nOE,
---      MIO_O0_nOE_i   => i_MIO_O0_nOE,
---      MIO_O1_nOE_i   => i_MIO_O1_nOE,
---
---      P_RnW_o        => im_P_RnW,
---      P_nRST_o       => im_P_nRST,
---      P_SER_TX_o     => im_P_SER_TX,
---      P_SER_RTS_o    => im_P_SER_RTS,
---
---      nADLC_o        => im_nADLC,
---      nKBPAWR_o      => im_nKBPAWR,
---      nIC32WR_o      => im_nIC32WR,
---      nPGFC_o        => im_nPGFC,
---      nPGFD_o        => im_nPGFD,
---      nFDC_o         => im_nFDC,
---      nTUBE_o        => im_nTUBE,
---      nFDCONWR_o     => im_nFDCONWR,
---      nVIAB_o        => im_nVIAB,
---
---      -- MIO_I0 phase
---
---      ser_cts_i      => imi_ser_cts,
---      ser_rx_i       => imi_ser_rx,
---      d_cas_i        => imi_d_cas,
---      kb_nRST_i      => imi_kb_nRST,
---      kb_CA2_i       => imi_kb_CA2,
---      netint_i       => imi_netint,
---      irq_i          => imi_irq,
---      nmi_i          => imi_nmi,
---
---      -- MIO_O1 phase
---      j_ds_nCS2_o    => im_j_ds_nCS2,
---      j_ds_nCS1_o    => im_j_ds_nCS1,
---      j_spi_clk_o    => im_j_spi_clk,
---      VID_HS_o       => im_VID_HS,
---      VID_VS_o       => im_VID_VS,
---      VID_CS_o       => im_VID_CS,
---      j_spi_mosi_o   => im_j_spi_mosi,
---      j_adc_nCS_o    => im_j_adc_nCS,
---
---      -- MIO_I1 phase
---      j_i0_i         => imi_j_i0_i,
---      j_i1_i         => imi_j_i1_i,
---      j_spi_miso_i   => imi_j_spi_miso_i,
---      btn0_i         => imi_btn0_i,
---      btn1_i         => imi_btn1_i,
---      btn2_i         => imi_btn2_i,
---      btn3_i         => imi_btn3_i,
---      kb_pa7_i       => imi_kb_pa7_i,
---
---      -- data phase
---      P_D_io         => im_P_D,
---
---      -- address phase
---      P_A_o          => im_P_A
---
---
---   );
+
+   e_brd_per:entity work.sim_peripherals_mux
+   port map (
+
+      clk_2MHz_E_i   => im_mux_mhz1E_clk,
+      clk_1MHz_E_i   => im_mux_mhz2E_clk,
+      
+      MIO_io         => im_mux_bus_io,
+      MIO_nALE_i     => im_mux_nALE_o,
+      MIO_D_nOE_i    => im_mux_D_nOE_o,
+      MIO_I0_nOE_i   => im_mux_I0_nOE_o,
+      MIO_I1_nOE_i   => im_mux_I1_nOE_o,
+      MIO_O0_nOE_i   => im_mux_O0_nOE_o,
+      MIO_O1_nOE_i   => im_mux_O1_nOE_o,
+
+      P_RnW_o        => ibpo_RnW,
+      P_nRST_o       => ibpo_nRST,
+      P_SER_TX_o     => ibpo_SER_TX,
+      P_SER_RTS_o    => ibpo_SER_RTS,
+
+      nADLC_o        => ibpo_nADLC,
+      nKBPAWR_o      => ibpo_nKBPAWR,
+      nIC32WR_o      => ibpo_nIC32WR,
+      nPGFC_o        => ibpo_nPGFC,
+      nPGFD_o        => ibpo_nPGFD,
+      nFDC_o         => ibpo_nFDC,
+      nTUBE_o        => ibpo_nTUBE,
+      nFDCONWR_o     => ibpo_nFDCONWR,
+      nVIAB_o        => ibpo_nVIAB,
+
+      -- MIO_I0 phase
+
+      ser_cts_i      => ibpi_ser_cts,
+      ser_rx_i       => ibpi_ser_rx,
+      d_cas_i        => ibpi_d_cas,
+      kb_nRST_i      => ibpi_kb_nRST,
+      kb_CA2_i       => ibpi_kb_CA2,
+      netint_i       => ibpi_netint,
+      irq_i          => ibpi_irq,
+      nmi_i          => ibpi_nmi,
+
+      -- MIO_O1 phase
+      j_ds_nCS2_o    => ibpo_j_ds_nCS2,
+      j_ds_nCS1_o    => ibpo_j_ds_nCS1,
+      j_spi_clk_o    => ibpo_j_spi_clk,
+      VID_HS_o       => ibpo_VID_HS,
+      VID_VS_o       => ibpo_VID_VS,
+      VID_CS_o       => ibpo_VID_CS,
+      j_spi_mosi_o   => ibpo_j_spi_mosi,
+      j_adc_nCS_o    => ibpo_j_adc_nCS,
+
+      -- MIO_I1 phase
+      j_i0_i         => ibpi_j_i0,
+      j_i1_i         => ibpi_j_i1,
+      j_spi_miso_i   => ibpi_j_spi_miso,
+      btn0_i         => ibpi_btn0,
+      btn1_i         => ibpi_btn1,
+      btn2_i         => ibpi_btn2,
+      btn3_i         => ibpi_btn3,
+      kb_pa7_i       => ibpi_kb_pa7,
+
+      -- data phase
+      P_D_io         => ibpio_P_D,
+
+      -- address phase
+      P_A_o          => ibpo_A
+
+
+   );
 --
 --   -- mock devices
 --
