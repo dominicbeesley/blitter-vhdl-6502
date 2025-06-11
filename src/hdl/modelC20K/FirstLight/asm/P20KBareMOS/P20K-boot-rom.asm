8b49ae893d0f3e1b315e24ab8467760707783aa3
dominicbeesley <dominicbeesley@gmail.com>
2025-06-11 14:59:26 +0100

    Tidy up and add proper boot message

--- src/hdl/modelC20K/FirstLight/asm/P20KBareMOS/P20K-boot-rom.asm
+++ src/hdl/modelC20K/FirstLight/asm/P20KBareMOS/P20K-boot-rom.asm
@@ -40,70 +40,14 @@ mos_handle_res:
 
 	jsr	noice_init
 
-	jsr	noice_enter
+	jmp	noice_enter
 
-@lp:	inc	CTR
-	bne	@s1
-	inc	CTR+1
-	bne	@s1
-	inc	CTR+2
-	bne	@s1
-	inc	CTR+3
-	
-@s1:	lda	CTR+3
-	jsr	PrintHexA
-	lda	CTR+2
-	jsr	PrintHexA
-	lda	CTR+1
-	jsr	PrintHexA
-	lda	CTR+0
-	jsr	PrintHexA
 
-	ldx	#0
-	ldy	#0
-@lp3:	jsr	wait
-	dex
-	bne	@lp3
-	dey
-	bne	@lp3
-	beq	@lp
-
-wait:	jsr	wait2
-wait2:	
+uart_tx:	
 	bit	UART_STAT
-	bmi	shch
-	rts
-
-shch:	pha
-	lda	UART_DAT
-	jsr	PrintHexA
-	pla
-	rts
-
-uart_tx:	bit	UART_STAT
 	bvs	uart_tx
 	sta	UART_DAT
 	rts
-
-
-PrintHexA:	pha
-		lsr	a
-		lsr	a
-		lsr	a
-		lsr	a
-		jsr	PrintHexNybA
-		pla
-		pha
-		jsr	PrintHexNybA
-		pla
-		rts
-PrintHexNybA:	and	#$0F
-		cmp	#10
-		bcc	@1
-		adc	#'A'-'9'-2
-@1:		adc	#'0'
-		jsr	uart_tx
-		rts
 
 
 mos_handle_nmi:
@@ -144,11 +88,11 @@ mos_handle_brk:	pla
 
 
 
-str_msg:	.byte "Hello Ishbel", 13, 10, 0
+str_msg:	.byte "NoIce Monitor MOS", 13, 10, 0
 
 		.SEGMENT "VECTORS"
-hanmi:  .addr   mos_handle_nmi                  ; FFFA 00 0D                    ..
-hares:  .addr   mos_handle_res                  ; FFFC CD D9                    ..
-hairq:  .addr   mos_handle_irq                  ; FFFE 1C DC                    ..
+hanmi:  .addr   mos_handle_nmi                  
+hares:  .addr   mos_handle_res                  
+hairq:  .addr   mos_handle_irq                  
 
 		.END
