95a46d45380cf45bcf06c64f1e8d6d8443f08eea
dominicbeesley <dominicbeesley@gmail.com>
2025-06-21 23:04:37 +0100

    c20K sram controller, include bb ram and flash ram

--- src/hdl/modelC20K/FirstLight/P20KBare/src/fb_C20K_MEM_SRAM.vhd
+++ src/hdl/modelC20K/FirstLight/P20KBare/src/fb_C20K_MEM_SRAM.vhd
@@ -24,32 +24,38 @@
 -- Company: 			Dossytronics
 -- Engineer: 			Dominic Beesley
 -- 
--- Create Date:    	16/04/2019
+-- Create Date:    	20/6/2025
 -- Design Name: 
 -- Module Name:    	fb_C20K_mem_sram
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
--- Description: 		A fishbone wrapper for C20K motherboard RAM
+-- Description: 		A fishbone wrapper for C20K motherboard RAM / EEPROM
 -- Dependencies: 
 --
 -- Revision: 
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
-
+-- TODO: harmonize with mk.3 blitter
 
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use std.textio.all;
-
 
 library work;
 use work.fishbone.all;
 
 entity fb_C20K_mem_sram is
+    generic (
+		SIM									: boolean := false;							-- skip some stuff, i.e. slow sdram start up
+		G_SWRAM_SLOT						: natural := 1;								-- when set address range $60 0000 - $7F 0000 goes to this MEM_nCE
+																									--  defaults to 1 for min_04 on rev.2
+		G_FAST_IS_10						: boolean := false;							-- when true run RAM(1..3) at 10ns
+		G_SLOW_IS_45						: boolean := false;							-- when true run BB RAM at 45ns else 55ns		
+		G_FLASH_IS_45						: boolean := false							-- when true run Flash at 45ns else 55ns
+	);
 	port(
 
 		-- fishbone signals
@@ -84,12 +90,6 @@ architecture rtl of fb_C20K_mem_sram is
 
 begin
 
---NOTE: simplified to use single CE1 chip at 10ns
-
-   mem_nCE_o(2)  <= '1';
-   mem_nCE_o(3)  <= '1';
-   mem_nCE_BB_o  <= '1';
-   mem_nCE_FL_o  <= '1';
 
 
 	fb_p2c_o.D_rd <= MEM_D_io;
@@ -111,7 +111,9 @@ begin
 			MEM_D_io <= (others => 'Z');
 			MEM_nOE_o <= '1';
 			MEM_nWE_o <= '1';
-			MEM_nCE_o(1) <= '1';
+			MEM_nCE_o <= (others => '1');
+			MEM_nCE_BB_o <= '1';
+			MEM_nCE_FL_o <= '1';
 			v_rdy_ctdn := RDY_CTDN_MAX;
 			r_rdy_ctdn <= RDY_CTDN_MIN;
 			r_ack <= '0';
@@ -127,7 +129,9 @@ begin
 						MEM_D_io <= (others => 'Z');
 						MEM_nOE_o <= '1';
 						MEM_nWE_o <= '1';
-						MEM_nCE_o(1) <= '1';
+						MEM_nCE_o <= (others => '1');
+						MEM_nCE_BB_o <= '1';
+						MEM_nCE_FL_o <= '1';
 						r_rdy <= '0';
 						v_rdy_ctdn := RDY_CTDN_MAX;	
 
@@ -138,14 +142,45 @@ begin
 							MEM_nWE_o <= not fb_c2p_i.we;
 							MEM_nOE_o <= fb_c2p_i.we;
 							MEM_A_o <= fb_c2p_i.A(20 downto 0);
+							if fb_c2p_i.we = '1' then
+								MEM_D_io <= fb_c2p_i.D_wr;
+							end if;
 
+							-- work out which memory chip and what speed
+							if fb_c2p_i.A(23) = '1' then
+								MEM_nCE_FL_o <= '0';
+								IF G_FLASH_IS_45 then
+									v_rdy_ctdn := to_unsigned(5, RDY_CTDN_LEN);
+								else
+									v_rdy_ctdn := to_unsigned(7, RDY_CTDN_LEN);
+								end if;
+							elsif fb_c2p_i.A(22 downto 21) = "11" then -- BBRAM
+								if (G_SWRAM_SLOT = 0) then
+									MEM_nCE_BB_o <= '0';
+									-- slow BB RAM...how slow?
+									if G_SLOW_IS_45 then
+										v_rdy_ctdn := to_unsigned(5, RDY_CTDN_LEN);
+									else
+										v_rdy_ctdn := to_unsigned(7, RDY_CTDN_LEN);
+									end if;
+								else
+									MEM_nCE_o(G_SWRAM_SLOT) <= '0';
+									if G_FAST_IS_10 then
+										v_rdy_ctdn := to_unsigned(1, RDY_CTDN_LEN);
+									else
+										v_rdy_ctdn := to_unsigned(2, RDY_CTDN_LEN);
+									end if;
+								end if;
+							else
 							-- ram at 0..$5F FFFF maps
-							MEM_nCE_o(1) <= '0';
---							if G_FAST_IS_10 then
+								MEM_nCE_o(to_integer(unsigned(fb_c2p_i.A(22 downto 21)))+1) <= '0';
+								if G_FAST_IS_10 then
 								v_rdy_ctdn := to_unsigned(1, RDY_CTDN_LEN);
---							else
---								v_rdy_ctdn := to_unsigned(2, RDY_CTDN_LEN);
---							end if;
+								else
+									v_rdy_ctdn := to_unsigned(2, RDY_CTDN_LEN);
+								end if;
+
+							end if;
 
 							if fb_c2p_i.we = '0' then							
 								state <= wait_rd;
