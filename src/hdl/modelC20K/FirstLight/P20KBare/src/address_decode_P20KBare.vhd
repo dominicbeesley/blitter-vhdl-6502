c5750b14966d7983152a11b37ddb57c6aac4e7c8
dominicbeesley <dominicbeesley@gmail.com>
2025-06-24 12:59:19 +0100

    c20k bare - flash eeprom from 8000-EFFF maps to 78000 - 7EFFF in flash

--- src/hdl/modelC20K/FirstLight/P20KBare/src/address_decode_P20KBare.vhd
+++ src/hdl/modelC20K/FirstLight/P20KBare/src/address_decode_P20KBare.vhd
@@ -77,7 +77,7 @@ begin
 			elsif addr_i(15 downto 8) = x"FC" or addr_i(15 downto 8) = x"FD" or addr_i(15 downto 8) = x"FE" then
 				peripheral_sel_o <= to_unsigned(PERIPHERAL_NO_SYS, peripheral_sel_o'length);
 				peripheral_sel_oh_o(PERIPHERAL_NO_SYS) <= '1';
-			elsif addr_i(15) = '1' then
+			elsif addr_i(15 downto 12) = x"F" then
 				peripheral_sel_o <= to_unsigned(PERIPHERAL_NO_MEM_ROM, peripheral_sel_o'length);
 				peripheral_sel_oh_o(PERIPHERAL_NO_MEM_ROM) <= '1';
 			elsif addr_i(15 downto 12) = x"0" then
