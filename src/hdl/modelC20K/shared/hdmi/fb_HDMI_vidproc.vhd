12025e7092e3307ba1b059bdc393fc880615cf6e
dominicbeesley <dominicbeesley@gmail.com>
2025-10-04 13:58:15 +0100

    reset nula on full/power reset

--- src/hdl/modelC20K/shared/hdmi/fb_HDMI_vidproc.vhd
+++ src/hdl/modelC20K/shared/hdmi/fb_HDMI_vidproc.vhd
@@ -181,6 +181,9 @@ begin
 	begin
 		if fb_syscon_i.rst = '1' then
 			r_reset_48 <= '1';
+			if fb_syscon_i.rst_state = resetfull or fb_syscon_i.rst_state = powerup then
+				r_reset_48_hard <= '1';
+			end if;
 		elsif rising_edge(CLK_48M_i) then
 			r_reset_48 <= '0';
 			r_reset_48_hard <= '0';
