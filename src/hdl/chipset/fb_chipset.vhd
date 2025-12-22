e1f8703ce60a95226f11c83ee882d2d9772275ce
dominicbeesley <dominicbeesley@gmail.com>
2025-12-22 19:40:34 +0000

    AERIS build for AndyC

--- src/hdl/chipset/fb_chipset.vhd
+++ src/hdl/chipset/fb_chipset.vhd
@@ -653,6 +653,12 @@ GNOSDCARD: IF NOT G_INCL_CS_SDCARD GENERATE
 END GENERATE;
 
 GSDCARD: IF G_INCL_CS_SDCARD GENERATE
+b_sdcard:block
+	signal	i_SPI_CS : std_logic_vector(7 downto 0);
+begin
+
+	SD_CS_o <= i_SPI_CS(0);
+
 	i_c2p_sdcard_per <= i_per_c2p_chipset(PERIPHERAL_NO_CHIPSET_SDCARD);
 	i_per_p2c_chipset(PERIPHERAL_NO_CHIPSET_SDCARD)	<=	i_p2c_sdcard_per;
 
@@ -665,7 +671,7 @@ GSDCARD: IF G_INCL_CS_SDCARD GENERATE
 	port map (
 
 		-- eeprom signals
-		SPI_CS_o(0)							=> SD_CS_o,
+		SPI_CS_o								=> i_SPI_CS,
 		SPI_CLK_o							=> SD_CLK_o,
 		SPI_MOSI_o							=> SD_MOSI_o,
 		SPI_MISO_i							=> SD_MISO_i,
@@ -677,7 +683,7 @@ GSDCARD: IF G_INCL_CS_SDCARD GENERATE
 		fb_c2p_i								=> i_c2p_sdcard_per,
 		fb_p2c_o								=> i_p2c_sdcard_per
 	);
-
+end block;
 
 END GENERATE;
 GNOEEPROM: IF NOT G_INCL_CS_EEPROM GENERATE
