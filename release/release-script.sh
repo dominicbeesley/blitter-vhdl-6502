6530eff29789e5da509933deb46e28441c26083d
dominicbeesley <8866586+dominicbeesley@users.noreply.github.com>
2026-03-29 18:19:03 +0100

    Update release-script.sh

--- release/release-script.sh
+++ release/release-script.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+set -e
+
 # make a release zip
 # expects built items in following locations
 
 BUILDDIR=./release
 
-mkdir -p ${BUILDDIR}
-mkdir -p ${BUILDDIR}/ssd
-mkdir -p ${BUILDDIR}/fpga
+CODE65=../../blitter-65xx-code
 
 HOSTFS=~/hostfs
-CODE65=/mnt/c/Users/Dominic/Documents/GitHub/blitter-65xx-code/build/ssds
+SSD65DIR=${CODE65}/build/ssds
 
 SSDS_65="roms65 tools65 demo65 adventure bigfonts examblit paula"
 SSDS="z80 bas816 roms69"
+
+PREBOOT=${CODE65}/build/roms/preboot
 
 ##ROMS65_SRC=${HOSTFS}/roms65
 ##ROMS65_ITEMS="ADFSH30.inf MOS120.M.inf BLMMFS.rom.inf BLTUTIL.inf OSTEST.M.inf basic2.rom.inf BASIC432.rom.inf SWMMFS.rom.inf"
@@ -34,11 +37,11 @@ SSDS="z80 bas816 roms69"
 ##BIGFONTS_OPT4=3
 
 ROMS69_SRC=${HOSTFS}/roms69
-ROMS69_ITEMS="6309BAS.bin.inf 6368BAS.bin.inf 6809BAS.bin.inf HOSTFS-myelin.bin.inf HOSTFS.bin.inf mosrom-6809.bin.inf mosrom-nat.bin.inf mosrom.bin.inf UTILS.bin.inf"
+ROMS69_ITEMS="BAS6309.R.inf BAS6368.R.inf BASIC.R.inf HOSTFS.R.inf HOSTFSM.R.inf MOS6309.M.inf MOS630N.M.inf MOS6809.M.inf UTILS09.R.inf"
 
-MODPLAY_SRC=${HOSTFS}/paula_demo
-MODPLAY_ITEMS="8BBAFOX.M.inf JAERP2K.M.inf MENU65.inf MODMENU.inf MODPLAY.inf ROOTYTO.M.inf THEFROG.M.inf WOUTERV.M.inf _21BOOT.inf"
-MODPLAY_OPT4=3
+#MODPLAY_SRC=${HOSTFS}/paula_demo
+#MODPLAY_ITEMS="8BBAFOX.M.inf JAERP2K.M.inf MENU65.inf MODMENU.inf MODPLAY.inf ROOTYTO.M.inf THEFROG.M.inf WOUTERV.M.inf _21BOOT.inf"
+#MODPLAY_OPT4=3
 
 Z80_SRC=${HOSTFS}/testz80
 Z80_ITEMS="FIRSTL.M.inf TEST.M.inf _21BOOT.inf _21FIRST.inf"
@@ -47,9 +50,18 @@ BAS816_SRC=${HOSTFS}/bas816_blit
 BAS816_ITEMS="BAS816.inf CLOCKSP.inf RUNB816.inf _21BOOT.inf"
 BAS816_OPT4=3
 
-rm ${BUILDDIR}/*
-rm ${BUILDDIR}/ssd/*
-rm ${BUILDDIR}/fpga/*
+if [[ -d "${BUILDDIR}" ]]; then
+	echo "Clear ${BUILDDIR}"
+	rm -R ${BUILDDIR}
+fi
+
+mkdir -p "${BUILDDIR}"
+mkdir -p "${BUILDDIR}/ssd"
+mkdir -p "${BUILDDIR}/fpga"
+mkdir -p "${BUILDDIR}/preboot"
+
+
+
 
 for ssd in ${SSDS}; do
 	echo "SSD: ${ssd}"
@@ -80,7 +92,7 @@ done
 
 for ssd in ${SSDS_65}; do
 
-	cp ${CODE65}/${ssd}.ssd ${BUILDDIR}/ssd/${ssd}.ssd
+	cp ${SSD65DIR}/${ssd}.ssd ${BUILDDIR}/ssd/${ssd}.ssd
 
 done;
 
@@ -91,7 +103,20 @@ for ssd in ${SSDS} ${SSDS_65}; do
 done;
 
 cp release-files.md ${BUILDDIR}
-cp /mnt/c/Users/Dominic/Documents/GitHub/blitter-vhdl-6502/src/hdl/mk2/boards/mk2/output_files/mk2blit.jic ${BUILDDIR}/fpga
-cp /mnt/c/Users/Dominic/Documents/GitHub/blitter-vhdl-6502/src/hdl/mk3/boards/cpu-16-max/output_files/mk3_16_max.pof ${BUILDDIR}/fpga
+cp ../src/hdl/mk2/boards/mk2/output_files/mk2blit.jic ${BUILDDIR}/fpga
+cp ../src/hdl/mk3/boards/cpu-16-max/output_files/mk3_16_max.pof ${BUILDDIR}/fpga
+cp ../src/hdl/modelC20K/boards/C20K/impl/pnr/C20K.fs ${BUILDDIR}/fpga
+cp ../src/hdl/modelC20K/boards/C20K816only/impl/pnr/C20K816only.fs ${BUILDDIR}/fpga
+cp ../src/hdl/modelC20K/boards/C20KFirstLight/impl/pnr/C20KFirstMON.fs ${BUILDDIR}/fpga
+cp ../src/hdl/modelC20K/boards/C20KFirstLight/impl/pnr/C20KFirstNoICE.fs ${BUILDDIR}/fpga
+
+chmod a+w ${BUILDDIR}/fpga/C20K.fs
+chmod a+w ${BUILDDIR}/fpga/C20K816only.fs
+chmod a+w ${BUILDDIR}/fpga/C20KFirstMON.fs
+chmod a+w ${BUILDDIR}/fpga/C20KFirstNoICE.fs
+
+cp ${PREBOOT}/romset-c20k.bin ${BUILDDIR}/preboot
+cp ${PREBOOT}/preboot2/c20k/preboot2.bin ${BUILDDIR}/preboot/preboot2-c20k.bin
+cp ${PREBOOT}/preboot-mk2.jic ${BUILDDIR}/preboot
 
 tar -cvzf release-$(date +%Y-%m-%d).tgz ${BUILDDIR}/*
